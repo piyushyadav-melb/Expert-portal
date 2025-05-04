0a8dd522f9cf5d96af44a61cd39af6eee3ef52e5
piyushyadav-melb <piyushmelb@gmail.com>
2025-05-04 20:44:17 +0530

    Updated

--- components/landing-page/hero.tsx
+++ components/landing-page/hero.tsx
@@ -3,10 +3,7 @@ import { Button } from "@/components/ui/button";
 
 const Hero = () => {
   return (
-    <section
-      className="bg-[url(https://dashboi-one.vercel.app/images/home/hero-bg.png)] bg-cover bg-no-repeat relative "
-      id="home"
-    >
+    <section className="" id="home">
       <div className="bg-white dark:bg-[#0F172A]">
         <div className="container">
           <div className=" relative z-10 h-screen flex items-center">
